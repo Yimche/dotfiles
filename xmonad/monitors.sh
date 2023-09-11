83ad2d23fa249630f9a23f30ea4f7c3a431a7ef6
Yimche <jcyinmo@gmail.com>
2023-09-11 18:17:40 +1000

    xmonad

--- xmonad/monitors.sh
+++ xmonad/monitors.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 if [[ $(xrandr -q | grep "^HDMI-1 connected" | wc -l) == 1 ]]; then
-  xrandr --output "eDP-1" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "HDMI-1" --mode 1280x1024 --pos 1920x0 --rotate normal 
+  xrandr --output "eDP-1" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "HDMI-1" --mode 1920x1080 --pos 1920x0 --rotate normal 
   $(xmobar -x 1 ~/.config/xmobar/xmobarrc1)
+  $(feh --bg-fill --no-fehbg ~/.config/wall.png &)
 elif [[ $(xrandr -q | grep "^DP-1 connected" | wc -l) == 1 ]]; then
   xrandr --output "eDP-1" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "DP-1" --mode 1280x1024 --pos 1920x0 --rotate normal 
   $(xmobar -x 1 ~/.config/xmobar/xmobarrc1)
+elif [[ $(xrandr -q | grep "1024x768_60.00" | wc -l) -ge 1 ]]; then
+  xrandr --output "DVI-I-1-1" --mode 1024x768_60.00 --right-of eDP-1
+  $(xmobar -x 1 ~/.config/xmobar/xmobarrc1)
+  $(feh --bg-fill --no-fehbg ~/.config/wall.png &)
+elif [[ $(xrandr -q | grep "1024x768_144.00" | wc -l) == 1 ]]; then
+  xrandr --output "DVI-I-1-1" --mode 1024x768_60.00 --right-of eDP-1
+  xmobar -x 1 ~/.config/xmobar/xmobarrc1
+  feh --bg-fill --no-fehbg ~/.config/wall.png
 else
   xrandr --output "eDP-1" --primary --mode 1920x1080 --pos 0x0 --rotate normal --output "HDMI-1" --off --output "DP-1" --off
 fi
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,14 +1,7 @@
---
--- xmonad example config file.
---
--- A template showing all available configuration hooks,
--- and how to override the defaults in your own xmonad.hs conf file.
---
--- Normally, you'd only override those defaults you care about.
---
-
 import XMonad
 import Data.Monoid
+import Data.Maybe
+import System.IO.Unsafe (unsafePerformIO)
 import System.Exit
 
 import XMonad.Layout.Hidden
@@ -35,21 +28,28 @@ import qualified XMonad.Actions.CycleWS as CWS
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
+import Colours
+
 myTerminal = "kitty"
 
--- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool 
 myFocusFollowsMouse = True
 
--- Whether clicking on a window to focus also passes the click to the window
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
--- Width of the window border in pixels.
---
 myBorderWidth  = 2
+
+
+-- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
+myWorkspaces    = ["dev","www","doc","chat","mus","vid","game","ext1"]
+--myWorkspaces    = ["1","2","3","4","5","6","7","8"]
+
+
+
+--------------------------------------------------------------------------------
+
+-- Key bindings. Add, modify or remove key bindings here.
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -58,17 +58,6 @@ myBorderWidth  = 2
 --
 myModMask  = mod4Mask
 
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
-myWorkspaces    = ["dev","www","doc","chat","mus","vid","game","ext1"]
-
--- Border colors for unfocused and focused windows, respectively.
-myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#4f97d7"
-
-
---------------------------------------------------------------------------------
-
--- Key bindings. Add, modify or remove key bindings here.
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- launch a terminal
@@ -157,6 +146,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     ]
     ++
 
+    -- Next Workspace, Previous Workspace
 
     [ ((modm              , xK_bracketright), CWS.nextWS)
     , ((modm              , xK_bracketleft) , CWS.prevWS)
@@ -198,9 +188,12 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((0 , 0x1008ff13), spawn "$HOME/code_Master/scripts/volume-notif.sh up")
     , ((0 , 0x1008ff03), spawn "$HOME/code_Master/scripts/brightness-notif.sh down")
     , ((0 , 0x1008ff02), spawn "$HOME/code_Master/scripts/brightness-notif.sh up")
-    , ((0 , 0x1008ff14), spawn "playerctl play-pause")
+    , ((0 , 0x1008ff14), spawn "$HOME/code_Master/scripts/fb2k.sh")
+		, ((0 , 0x1008ff17), spawn "playerctl next")
+    , ((0 , 0x1008ff16), spawn "playerctl previous")
     , ((0 , 0xff61)    , spawn "flameshot gui")
     ]
+
 
 --------------------------------------------------------------------------------
 
@@ -224,21 +217,20 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- Layouts:
 --myLayout = toggleLayouts (noBorders Full) (avoidStruts $ myBorders $ tiled ||| Mirror tiled ||| Full)
 myLayout = hiddenWindows $ toggleLayouts (noBorders Full) (avoidStruts $ myBorders $ tiled ||| Full)
-  where
-     -- default tiling algorithm partitions the screen into two panes
+  where -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
 
      -- The default number of windows in the master pane
      nmaster = 1
 
      -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+     ratio   = 3/5
 
      -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+     delta   = 5/100
      
      -- Borders
-     myBorders = spacingRaw False (Border 10 3 10 10) True (Border 5 5 5 5) True
+     myBorders = spacingRaw False (Border 3 3 10 10) True (Border 5 5 5 5) True
 
 -- Window rules:
 myManageHook = composeAll
@@ -246,10 +238,12 @@ myManageHook = composeAll
     , className =? "Gimp"           --> doFloat
     , className =? "Wine"           --> doFloat
     , className =? "copyq"          --> doFloat
+    , title     =? "foobar2000 v2.0" --> doFloat
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore
     , className =? "discord"        --> doShift "chat"
     , className =? "Wine"           --> doShift "chat"
+    , className =? "foobar2000.exe" --> doShift "mus"
     ]
 
 -- Event handling
@@ -260,19 +254,24 @@ myLogHook = return ()
 
 -- Startup hook
 myStartupHook = do
-  spawnOnce "picom &"
+  spawnOnce "picom --experimental-backends &"
   spawnOnce "xrdb ~/.Xresources &"
   spawnOnce "feh --bg-fill --no-fehbg ~/.config/wall.png &"
   spawnOnce "betterlockscreen -u ~/.config/wall.png &"
   spawnOnce "easyeffects --gapplication-service &"
   spawnOnce "dunst &"
-  spawnOnce "exec /usr/bin/trayer --edge top --align right --SetDockType true --SetPartialStrut true --widthtype request --expand true --transparent true --alpha 0 --tint 000000 --height 32 &"
+  --spawnOnce "exec /usr/bin/trayer --edge top --distance 15 --align right --distancefrom right --SetDockType true --SetPartialStrut true --widthtype request --expand true --transparent true --alpha 0 --tint 000000 --height 29 &"
   spawnOnce "fcitx &"
-  spawnOnce "birdtray &"
   spawnOnce "copyq &"
-  spawnOnce "sleep 3 && blueberry-tray &"
+  spawnOnce "python /home/julian/code_Master/Last.fm-Discord-Rich-Presence/main.py"
+  spawnOnce "xrandr --setprovideroutputsource 1 0 --setprovideroutputsource 2 0 &"
   spawn "exec ~/.config/xmonad/monitors.sh"
 
+-- # of windows on workspace
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
+-- Main Process
 main = do
   xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc"
   xmonad $ ewmhFullscreen 
@@ -309,7 +308,7 @@ defaults = def {
 
 myXmobarPP :: PP
 myXmobarPP = def
-    { ppSep             = xmobarMagenta " • "
+    { ppSep             = xmobarMagenta " <fn=2>\xf054</fn> "
     , ppTitleSanitize   = xmobarStrip
     , ppCurrent         = xmobarWhite . wrap " " "" . xmobarBorder "Bottom" blue    3 . wrap "" "" 
 		, ppVisible         = xmobarWhite . wrap " " "" . xmobarBorder "Bottom" yellow  3 . wrap "" ""
@@ -317,11 +316,12 @@ myXmobarPP = def
     , ppHiddenNoWindows = xmobarLowWhite . wrap " " ""
     , ppUrgent          = xmobarRed . wrap (xmobarYellow "!") (xmobarYellow "!")
     , ppOrder           = \[ws, l, _, wins] -> [ws, wins]
-    , ppExtras          = [logTitles formatFocused formatUnfocused]
+		--, ppExtras          = [logTitles formatFocused formatUnfocused]
+		, ppExtras          = [windowCount]
     }
   where
-    formatFocused   = wrap (xmobarWhite    "[") (xmobarWhite    "]") . xmobarMagenta . ppWindow
-    formatUnfocused = wrap (xmobarLowWhite "[") (xmobarLowWhite "]") . xmobarBlue    . ppWindow
+    formatFocused   = wrap (xmobarWhite    "[") (xmobarWhite    "]") . xmobarBlue    . ppWindow
+    formatUnfocused = wrap (xmobarLowWhite "[") (xmobarLowWhite "]") . xmobarMagenta . ppWindow
 
     -- | Windows should have *some* title, which should not not exceed a
     -- sane length.
@@ -336,10 +336,16 @@ myXmobarPP = def
     xmobarRed      = xmobarColor red      ""
     xmobarLowWhite = xmobarColor lowWhite ""
 
-    blue, lowWhite, magenta, red, white, yellow :: String
-    magenta  = "#a31db1"
-    blue     = "#4f97d7"
-    white    = "#f8f8f8"
-    yellow   = "#ffa500"
-    red      = "#f2241f"
-    lowWhite = "#b8b8b8"
+blue, lowWhite, magenta, orange, red, white, yellow :: String
+blue     = "#4f97d7"
+lowWhite = "#b8b8b8"
+magenta  = "#a31db1"
+orange   = "#ffa500"
+red      = "#f2241f"
+white    = "#f8f8f8"
+yellow   = "#ebbf83"
+
+-- Border colors for unfocused and focused windows, respectively.
+myNormalBorderColor  = "#263238"
+myFocusedBorderColor = "#88c0d0"
+--myFocusedBorderColor = "#4f97d7"
