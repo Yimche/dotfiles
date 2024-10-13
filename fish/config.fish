138dc003975934aa205e083d084748ab0ea1c459
Yimche <jcyinmo@outlook.com>
2024-10-14 00:46:31 +1100

    updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -10,18 +10,19 @@ end
 
 fish_vi_key_bindings
 
-alias webcam='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
+alias diff='diff -W $(tput cols) --color=always'
 alias irc='ssh ircj@akropolis.trickypr.com'
 alias lf='lfcd'
 alias fd='fd -H -i'
-alias vim='nvim'
 alias nps='cat /sys/bus/pci/devices/0000:00:01.0/power_state'
+alias vim='nvim'
 alias vm='kitty +kitten ssh comp2700-lab'
+alias webcam='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
 
 starship init fish | source
 zoxide init fish | source
 
-set -gx LS_COLORS (vivid generate catppuccin-mocha)
+set -gx LS_COLORS (vivid generate catppuccin-macchiato)
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
 set -gx PATH $HOME/.cabal/bin /home/julian/.ghcup/bin $PATH # ghcup-env
