a4dc415050b7a6653d9e055e0cf5498a5289acd7
Yimche <jcyinmo@gmail.com>
2025-10-28 01:33:48 +1100

    updated waybar

--- waybar/scripts/cava.sh
+++ waybar/scripts/cava.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-is_cava_ServerExist=$(ps -ef | grep -m 1 cava | grep -v "grep" | wc -l)
-if [ "$is_cava_ServerExist" = "0" ]; then
-  echo "cava_server not found" >/dev/null 2>&1
-  exit
-elif [ "$is_cava_ServerExist" = "1" ]; then
-  killall cava
+
+CAVA_CMD="cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'"
+
+if [[ $1 =~ "toggle" ]]; then
+    CAVA_PID="$(pgrep -x cava)"
+    if [[ $CAVA_PID > 0 && $(ps -o stat= -C cava | grep -c T) == 0 ]]; then
+        kill -19 "$CAVA_PID"
+        echo "paused\n"
+    else
+        kill -18 "$CAVA_PID"
+        echo "continue\n"
+    fi
+    exit
 fi
 
-cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
+killall cava
+eval "$CAVA_CMD"
