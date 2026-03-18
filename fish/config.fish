840c4040e9b0967cbb1ea81f640fa9134c095f5c
Yimche <jcyinmo@gmail.com>
2026-03-18 17:10:48 +1100

    fish things

--- fish/config.fish
+++ fish/config.fish
@@ -2,59 +2,11 @@
 
 # overwrite greeting
 # potentially disabling fastfetch
-#function fish_greeting
-#    printf %s\n ""
-#    fastfetch
-#end
+function fish_greeting
+end
+
 
 fish_vi_key_bindings
-
-#alias diff='diff -W $(tput cols) --color=always'
-#alias irc='ssh ircj@akropolis.trickypr.com'
-#alias lf='lfcd'
-#alias fd='fd -H -i'
-#alias nps='cat /sys/bus/pci/devices/0000:00:01.0/power_state'
-#alias vim='nvim'
-#alias hx='helix'
-#alias webcam='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
-#alias objdump='objdump --disassembler-color=colour'
-#
-### Useful aliases
-## Replace ls with eza
-#alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-#alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
-#alias ll='eza -l --color=always --group-directories-first --icons' # long format
-#alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
-#alias l.="eza -a | grep -e '^\.'" # show only dotfiles
-#
-## Common use
-#alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-#alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-#alias tarnow='tar -acf '
-#alias untar='tar -zxvf '
-#alias wget='wget -c '
-#alias psmem='ps auxf | sort -nr -k 4'
-#alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-#alias vdir='vdir --color=auto'
-#alias grep='grep --color=auto'
-#alias hw='hwinfo --short' # Hardware Info
-#alias big="expac -H M '%m\t%n' | sort -h | nl" # Sort installed packages according to size in MB
-#alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
-#alias update='sudo pacman -Syu'
-#
-## Get fastest mirrors
-#alias mirror="sudo cachyos-rate-mirrors"
-#
-## Cleanup orphaned packages
-#alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-#
-## Get the error messages from journalctl
-#alias jctl="journalctl -p 3 -xb"
-#
-## Recent installed packages
-#alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-
-# Replaced aliases with functions
 
 function diff
     command diff -W (tput cols) --color=always $argv
@@ -92,9 +44,6 @@ function objdump
     command objdump --disassembler-color=colour $argv
 end
 
-## Useful functions
-
-# Replace ls with eza
 function ls
     eza -al --color=always --group-directories-first --icons $argv
 end
@@ -114,8 +63,6 @@ end
 function l.
     eza -a | grep -e '^\.' $argv
 end
-
-# Common use
 
 function grubup
     sudo grub-mkconfig -o /boot/grub/grub.cfg $argv
@@ -185,23 +132,6 @@ function rip
     expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl $argv
 end
 
-# Existing functions
-
-function vm
-    qemu-system-x86_64 -enable-kvm -m 4G -smp 2 -nic user,hostfwd=tcp::5555-:22 ~/Documents/uni/Y3S1/3703/comp3703-2025.qcow2 &
-    kitty +kitten ssh comp3703
-    kill %1
-end
-
-function yy
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
-end
-
 function pyenv
     command pyenv init - | source
 end
@@ -259,8 +189,8 @@ function clear
 end
 
 function vm
-    qemu-system-x86_64 -enable-kvm -m 4G -smp 2 -nic user,hostfwd=tcp::5555-:22 ~/Documents/uni/Y3S1/3703/comp3703-2025.qcow2 &
-    kitty +kitten ssh comp3703
+    qemu-system-x86_64 -enable-kvm -m 4G -smp 2 -nic user,hostfwd=tcp::5555-:22 ~/.local/share/libvirt/images/vrex.qcow2 &
+    kitty +kitten ssh vrex
     kill %1
 end
 
